194864982d0bf1970ab014d0819081f6eaee0c0c
Rajan Puri <98840946+purirajan@users.noreply.github.com>
2022-02-01 13:52:29 -0500

    Create prime_factors.py

---
+++ prime_factors.py
@@ -0,0 +1,17 @@
+def print_prime_factors(number):
+  # Start with two, which is the first prime
+  factor = 2
+  # Keep going until the factor is larger than the number
+  while factor <= number:
+    # Check if factor is a divisor of number
+    if number % factor == 0:
+      # If it is, print it and divide the original number
+      print(factor)
+      number = number / factor
+    else:
+      factor =factor +1
+      # If it's not, increment the factor by one
+      ___
+  return "Done"
+
+print_prime_factors(300000)
